9bb154c1de33f9c4168ea0cb2589e3118e9f0529
daniil <zinindaniil11@gmail.com>
2023-01-13 22:36:40 +0600

    10 домашняя работа

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,12 @@
 package org.example;
 
-import org.example.model.StarTriangle;
 
-import java.util.Scanner;
+import org.example.enums.Colors;
 
 public class Main {
     public static void main(String[] args) {
-        StarTriangle starTriangle = new StarTriangle(5);
-        System.out.println(starTriangle);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите сторону квадрата: ");
-        Double squareSide = scanner.nextDouble();
-
-        System.out.println(squareArea(squareSide));
-        System.out.println(squarePerimeter(squareSide));
-        System.out.println(squareDiagonal(squareSide));
-    }
-
-    public static double squareArea(double squareSide) {
-        return squareSide * squareSide;
-    }
-
-    public static double squarePerimeter(double squareSide) {
-        return squareSide * 4;
-    }
-
-    public static double squareDiagonal(double squareSide) {
-        return Math.sqrt(2)  * squareSide;
+        // 1.
+        System.out.println(Colors.BLUE);
     }
 }
